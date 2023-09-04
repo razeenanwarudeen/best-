8f62ad767d978cb17f5facc4af9ebc8b01d195a4
razeenanwarudeen <144059496+razeenanwarudeen@users.noreply.github.com>
2023-09-04 10:57:38 -0800

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,39 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64f617c26efcea001ae662cc",
+				"64f617c26efcea001ae662c8"
+			]
+		},
+		"64f617c26efcea001ae662c8": {
+			"id": "64f617c26efcea001ae662c8",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"64f617c26efcea001ae662cc": {
+			"id": "64f617c26efcea001ae662cc",
+			"name": "404",
+			"pageUrl": "404"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {
+			"title": "You choose are great vacation",
+			"description": "Anyone can build a vacation, we build experiences",
+			"background_win10": "#0CAD1C",
+			"favicon_32px": "https://uploads.quarkly.io/614ce40335c5c8001f7746e0/images/favicon.png?v=2021-09-23T20:33:18.314Z",
+			"favicon_win10_270px": "https://uploads.quarkly.io/614ce40335c5c8001f7746e0/images/270.png?v=2021-09-23T20:33:32.819Z",
+			"og:description": "Anyone can build a vacation, we build experiences",
+			"og:title": "You choose are great vacation",
+			"og:image": "https://uploads.quarkly.io/614ce40335c5c8001f7746e0/images/OGimage.png?v=2021-09-23T20:32:10.363Z",
+			"favicon_apple_152px": "https://uploads.quarkly.io/614ce40335c5c8001f7746e0/images/32x32.png?v=2021-09-23T20:33:26.450Z"
+		}
+	}
+}
